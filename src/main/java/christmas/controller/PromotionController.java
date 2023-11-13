5a8cf2ccef6962380a775a74e6610e1ab24c4f1e
Daseul <daseull.rep@gmail.com>
2023-11-13 12:50:45 +0900

    feat(PromotionController): 스켈레톤 코드 구현

---
+++ src/main/java/christmas/controller/PromotionController.java
@@ -0,0 +1,25 @@
+package christmas.controller;
+
+public class PromotionController {
+
+    public void run(){
+        askVisitDate();
+        askMenus();
+        showPlan();
+    }
+
+    //TODO
+    private void askVisitDate() {
+
+    }
+
+    //TODO
+    private void askMenus() {
+
+    }
+
+    //TODO
+    private void showPlan() {
+
+    }
+}
