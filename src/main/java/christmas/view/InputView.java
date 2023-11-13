49b648bcd2cc54f45e87917c0550459986f6876f
Daseul <daseull.rep@gmail.com>
2023-11-13 12:44:19 +0900

    feat(InputView): 메뉴를 입력받는 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,13 +1,20 @@
 package christmas.view;
 
 import static christmas.view.Parser.parseDate;
+import static christmas.view.Parser.parseMenus;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class InputView {
 
     public int readVisitDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         return parseDate(Console.readLine());
+    }
+
+    public List<String> readMenus() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        return parseMenus(Console.readLine(), ",");
     }
 }
--- src/main/java/christmas/view/Parser.java
+++ src/main/java/christmas/view/Parser.java
@@ -1,9 +1,15 @@
 package christmas.view;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Parser {
 
     public static int parseDate(String input) {
         return Integer.parseInt(input);
     }
 
+    public static List<String> parseMenus(String input, String delimiter) {
+        return Arrays.stream(input.split(delimiter)).toList();
+    }
 }
